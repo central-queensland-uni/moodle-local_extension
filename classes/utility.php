4b654e0b3938a1c80ea4dcf2cc17a4509bc9bd59
Kristian Ringer <kristian.ringer@catalyst-au.net>
2019-04-24 10:13:51 +1000

    Fixed an error in debugging

--- classes/utility.php
+++ classes/utility.php
@@ -93,7 +93,8 @@ class utility {
 
         echo count($courses) . " courses found in calendar set filters. ";
         if (count($courses) == 1) {
-            echo "Only found this one course with courseid $courses[0]";
+            echo "Only found this one course with courseid";
+            var_dump($courses);
         }
 
         $allevents = calendar_get_events($start, $end, [$userid], $groups, $courses);
